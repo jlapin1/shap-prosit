3f0a9c9c3cffcca7aef9e886f694f2290e921896
Joel Lapin <joellapin@comcast.net>
2026-08-14 14:40:55 +0200

    Replaced pkg_resources with importlib

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,9 @@
-import pkg_resources
-installed_packages = pkg_resources.working_set
-package_key = [m for m in installed_packages.entry_keys.keys() if 'site-packages'==m.split('/')[-1]][0]
-installed_packages = installed_packages.entry_keys[package_key]
+#import pkg_resources
+#installed_packages = pkg_resources.working_set
+#package_key = [m for m in installed_packages.entry_keys.keys() if 'site-packages'==m.split('/')[-1]][0]
+#installed_packages = installed_packages.entry_keys[package_key]
+import importlib
+installed_packages = importlib.metadata.packages_distributions().keys()
 if 'torch' in installed_packages:
     from .peptide_encoder import PeptideEncoderModel
     from .prosit import PrositModel
